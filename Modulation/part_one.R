59ebf9052aa7cc090f247a2f02311c8aff434a9c
Ared Icus <aredicus@192.168.1.3>
2023-05-13 02:17:48 +0300

    modulation

--- Modulation/part_one.R
+++ Modulation/part_one.R
@@ -6,8 +6,15 @@ myModuleUI <- function(id, label = "Input text: ") {
     
     textInput(ns("txt"), label),
     
-    textOutput(ns("result"))
+    textOutput(ns("result")),
     
+    sliderInput(ns("slide"), "numbers", 10, 100, 50),
+    
+    numericInput(ns("mean"), "Mean", min = 1, max = 100, value = 70),
+    
+    numericInput(ns("sd"), "STD", min = 1, max = 100, value = 70),
+    
+    highchartOutput(ns("myfirst_highchart"))
   )
   
 }
@@ -19,11 +26,32 @@ myModule <- function(input, output, session, prefix = "My first output module") 
   output$result <- renderText({
     
     paste0(
-      prefix,
-      ": ",
-      toupper(input$txt),
-      tolower(input$txt)
+      "Ты ввел: ",
+      input$txt,
+      ", а на слайдаре: ",
+      input$slide
     )
+    
+  })
+  
+  output$myfirst_highchart <- renderHighchart({
+    df <-  data.frame(
+      x = rnorm(
+        input$slide,
+        input$mean,
+        input$sd
+      ),
+      y = rnorm(input$slide,5,30)
+    )
+    hchart(
+      df,
+      "point",
+      hcaes(
+        x = x,
+        y = y
+      )
+    )
+    
     
   })
   
@@ -43,9 +71,9 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
   
-  callModule(myModule, "myModule1", prefix = "Converted to uppercase") #
+  callModule(myModule, "myModule1", prefix = "First slider") #
   
-  callModule(myModule, "myModule2", prefix = "My mondule 3") #
+  callModule(myModule, "myModule2", prefix = "Second slider") #
   
 }
 
